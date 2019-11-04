df129b5edc945da93dcc07c13dc0c12e2a8c39cb
Wspsxing <biluohc@qq.com>
2019-11-05 00:04:37 +0800

    add References

--- ac-example/src/lib.rs
+++ ac-example/src/lib.rs
@@ -1,3 +1,14 @@
+/*!
+
+# References
+* https://github.com/upsuper/custom-derive-2019/blob/master/script.zh.md
+* https://crates.io/crates/proc-macro2
+* https://crates.io/crates/syn
+* https://crates.io/crates/quote
+* https://crates.io/crates/heck
+* https://crates.io/crates/darling
+
+*/
 #[macro_use]
 extern crate ac_derive;
 
